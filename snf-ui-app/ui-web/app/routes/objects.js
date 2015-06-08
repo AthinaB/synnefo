76318613e717240d39bcefd454d564c7fcfe5c25
Athina Bekakou <athinabek@grnet.gr>
2015-06-08 16:40:22 +0300

    Fix bug regarding empty containers

--- snf-ui-app/ui-web/app/routes/objects.js
+++ snf-ui-app/ui-web/app/routes/objects.js
@@ -92,6 +92,7 @@ export default Ember.Route.extend(ResetScrollMixin,{
                 return objects;
                 });
             }
+            return objects;
         }
         // if there are items
         else {
